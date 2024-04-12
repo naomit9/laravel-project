5e3ab22e54a69420bd77cbc1ef891ea834aab29d
nhutr <nhutr@LAPTOP-EJRNGLNE>
2024-04-12 16:46:08 -0400

    first commit

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+    <div class="note-container">
+        <a href="#" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                            {{ $note->note }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="#" class="note-edit-button">View</a>
+                        <a href="#" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    <div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</x-layout>
